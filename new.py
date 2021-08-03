b2045351563d34540affbc4ef70f15d46b8dc345
ራሚኖ <64677074+rami-star@users.noreply.github.com>
2021-08-03 19:20:09 +0200

    Add files via upload

---
+++ new.py
@@ -0,0 +1,12 @@
+import re
+
+fhand = open('regex_sum_1278144.txt')
+tosum = 0
+
+for line in fhand:
+	numlist = re.findall('[0-9]+',line)
+	if len(numlist) > 0:
+		for item in numlist:
+			tosum = tosum + int(item)
+print(tosum)
+	
